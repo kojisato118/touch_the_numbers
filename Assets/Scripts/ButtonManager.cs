3b02a5091866864b38b3eca321f911f284199a90
ksato <ksato@miew.co.jp>
2015-02-06 00:54:04 +0900

    25マスをランダムに配置&1から順に決していける

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class ButtonManager : MonoBehaviour
+{
+		//プロパティ
+		public event EventHandler<ButtonActionEventArgs> SelecetedButton;
+
+		public Text ButtonText;
+
+		public int Number;
+
+		// Use this for initialization
+		void Start ()
+		{
+				ButtonText.text = Number.ToString ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		//Action
+		public void ClickButton(){
+				RaiseSelectedButton (this.Number);
+		}
+
+		//Event
+		protected void RaiseSelectedButton (int selectedButtonNumber)
+		{
+				var e = new ButtonActionEventArgs (selectedButtonNumber);
+				if (SelecetedButton != null)
+						SelecetedButton (this, e);
+		}
+}
+
+
+public class ButtonActionEventArgs : EventArgs
+{
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="ButtonActionEventArgs"/> class.
+		/// </summary>
+		/// <param name="number">Number.</param>
+		/// 
+		public ButtonActionEventArgs (int number)
+		{
+				this.number = number;
+		}
+
+		/// <summary>
+		/// Gets the number.
+		/// </summary>
+		/// <value>The number.</value>
+		public int number { get; private set; }
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+		public GameObject ButtonPrefab;
+		public GameObject Panel;
+
+		private int[] NumbersList;
+
+		private int AnsNumber;
+
+		// Use this for initialization
+		void Start ()
+		{
+				AnsNumber = 1;
+				CreateNumbersList ();
+
+				for (int i = 0; i < 25; i++) {
+						GameObject buttonObj = Instantiate (ButtonPrefab) as GameObject;
+						buttonObj.transform.SetParent (Panel.transform);
+						ButtonManager buttonManager = buttonObj.GetComponent<ButtonManager> ();
+						buttonManager.Number = NumbersList[i] + 1;
+						buttonManager.SelecetedButton += SelectedButton;
+				}
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		void CreateNumbersList ()
+		{
+				int number = 25;
+				NumbersList = new int[number];
+
+				for (int i = 0; i < number; i++)
+						NumbersList [i] = i;
+
+				for (int i = number; i > 1; i--) {
+						int p = Random.Range(0, i);
+						int t = NumbersList [i - 1];
+						NumbersList [i - 1] = NumbersList [p];
+						NumbersList [p] = t;
+				}
+
+		}
+
+		void SelectedButton (object sender, ButtonActionEventArgs e)
+		{
+				if (e.number == AnsNumber) {
+						ButtonManager buttonManager = (ButtonManager)sender;
+						buttonManager.transform.localScale = new Vector3 (0, 0, 0);
+
+						AnsNumber++;
+				}
+		}
+}
