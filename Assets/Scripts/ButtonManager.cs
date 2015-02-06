8c32280a83800b5981ee12cdcb60c9aa3f814693
ksato <ksato@miew.co.jp>
2015-02-06 11:35:20 +0900

    gitテスト用に一旦commit

--- Assets/Scripts/ButtonManager.cs
+++ Assets/Scripts/ButtonManager.cs
@@ -5,57 +5,64 @@ using System;
 
 public class ButtonManager : MonoBehaviour
 {
-		//プロパティ
-		public event EventHandler<ButtonActionEventArgs> SelecetedButton;
+	//プロパティ
+	public event EventHandler<ButtonActionEventArgs> SelecetedButton;
 
-		public Text ButtonText;
+	public Text ButtonText;
 
-		public int Number;
+	public int Number;
 
-		// Use this for initialization
-		void Start ()
-		{
-				ButtonText.text = Number.ToString ();
-		}
+	// Use this for initialization
+	void Start ()
+	{
+		ButtonText.text = Number.ToString ();
+	}
 	
-		// Update is called once per frame
-		void Update ()
-		{
+	// Update is called once per frame
+	void Update ()
+	{
 	
-		}
+	}
 
-		//Action
-		public void ClickButton(){
-				RaiseSelectedButton (this.Number);
-		}
+	//Action
+	public void ClickButton ()
+	{
+		RaiseSelectedButton (this.Number);
+	}
 
-		//Event
-		protected void RaiseSelectedButton (int selectedButtonNumber)
-		{
-				var e = new ButtonActionEventArgs (selectedButtonNumber);
-				if (SelecetedButton != null)
-						SelecetedButton (this, e);
-		}
+	public void HideButton ()
+	{
+		GetComponentInParent<Animator> ().SetBool ("Visible", false);
+
+	}
+
+	//Event
+	protected void RaiseSelectedButton (int selectedButtonNumber)
+	{
+		var e = new ButtonActionEventArgs (selectedButtonNumber);
+		if (SelecetedButton != null)
+			SelecetedButton (this, e);
+	}
 }
 
 
 public class ButtonActionEventArgs : EventArgs
 {
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="ButtonActionEventArgs"/> class.
-		/// </summary>
-		/// <param name="number">Number.</param>
-		/// 
-		public ButtonActionEventArgs (int number)
-		{
-				this.number = number;
-		}
+	/// <summary>
+	/// Initializes a new instance of the <see cref="ButtonActionEventArgs"/> class.
+	/// </summary>
+	/// <param name="number">Number.</param>
+	/// 
+	public ButtonActionEventArgs (int number)
+	{
+		this.number = number;
+	}
 
-		/// <summary>
-		/// Gets the number.
-		/// </summary>
-		/// <value>The number.</value>
-		public int number { get; private set; }
+	/// <summary>
+	/// Gets the number.
+	/// </summary>
+	/// <value>The number.</value>
+	public int number { get; private set; }
 
 }
